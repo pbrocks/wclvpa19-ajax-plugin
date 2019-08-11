27e6b874c3c8ef1b26753b11a1efc4d1bb456486
Paul Barthmaier <paul@barthmaier.rocks>
2019-08-11 09:11:21 -0400

    i18n

--- wclvpa19-ajax-plugin.php
+++ wclvpa19-ajax-plugin.php
@@ -6,6 +6,7 @@
  * Version: 1.1.2
  * Author URI: https://github.com/pbrocks
  * Text Domain: wclvpa19-ajax-plugin
+ * Domain Path: /languages
  */
 
 /**
@@ -14,9 +15,22 @@
 defined( 'ABSPATH' ) || die( 'File cannot be accessed directly' );
 
 // echo '<h2>' . __FILE__ . '</h2>';
-/**
- * This line includes all php files located in the /inc folder. If there is a problem with the code in the file, you can comment this line out by placing two forward slashes '//' in front of the require statement which turns off all code in that directory.
- */
-foreach ( glob( __DIR__ . '/inc/*.php' ) as $filename ) {
-	require $filename;
+if ( file_exists( __DIR__ . '/inc' ) && is_dir( __DIR__ . '/inc' ) ) {
+	/**
+	 * This line includes all php files located in the /inc folder. If there is a problem with the code in the file, you can comment this line out by placing two forward slashes '//' in front of the require statement which turns off all code in that directory.
+	 */
+	foreach ( glob( __DIR__ . '/inc/*.php' ) as $filename ) {
+		require $filename;
+	}
 }
+
+
+/**
+ * Setup WordPress localization support
+ *
+ * @since 4.0
+ */
+function wclvpa19_ajax_plugin_load_textdomain() {
+	load_plugin_textdomain( 'wclvpa19-ajax-plugin', false, basename( dirname( __FILE__ ) ) . '/languages' );
+}
+add_action( 'plugins_loaded', 'wclvpa19_ajax_plugin_load_textdomain' );
