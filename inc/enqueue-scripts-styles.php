52d76703b6ae25ab37fa744ad3f5482617abf3f8
Paul Barthmaier <pbrocks@users.noreply.github.com>
2020-12-26 12:18:20 -0500

    update login form styling

--- inc/enqueue-scripts-styles.php
+++ inc/enqueue-scripts-styles.php
@@ -5,7 +5,7 @@
  * Keeping files distinct according to their function is a way of maintaining sanity for developers. If there is an issue with a given file, its functions can be turned of in an instant by commenting out the line of code in the base file that includes this file in the plugin.
  */
 
-add_action( 'login_enqueue_scripts', 'enqueue_login_scripts_styles' );
+add_action( 'login_enqueue_scripts', 'enqueue_login_scripts_styles', 11 );
 function enqueue_login_scripts_styles() {
 	wp_register_style( 'login-page', plugins_url( 'css/login-style.css', __FILE__ ) );
 	wp_enqueue_style( 'login-page' );
